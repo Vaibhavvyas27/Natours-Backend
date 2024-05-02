d1d1f6b80e1c962a9d2614b16e4d9552f8ac7e8e
Vaibhavvyas27 <vaibhavvyas2005@gmail.com>
2024-05-02 15:44:56 +0530

    Set Cookies to samesite:none

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,7 +63,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
     res.cookie('jwt_cookie', token, {
         expires : new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        // sameSite: 'None',
+        sameSite: 'None',
         httpOnly : true
     }).status(201).json({
         status: 'Success',
