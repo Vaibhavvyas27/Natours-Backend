ef6829e2aab13f4b74c6f463c123630777fb35c0
Vaibhavvyas27 <vaibhavvyas2005@gmail.com>
2024-05-03 11:47:39 +0530

    Add url in psyments

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,12 +9,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRETE_KEY)
 
 
 exports.getCheckoutSessions = catchAsync(async (req, res, next) => {
+    console.log(req.get('origin'))
     const tour = await Tour.findById(req.params.tourId);
     // Create Checkout Session 
     const session = await stripe.checkout.sessions.create({
         payment_method_types : ['card'],
-        success_url : `${req.protocol}://dev-natours-vv.netlify.app/tour/success?tour_id=${req.params.tourId}`,
-        cancel_url : `${req.protocol}://dev-natours-vv.netlify.app/tour/${tour.slug}`,
+        success_url : `${req.get('origin')}/tour/success?tour_id=${req.params.tourId}`,
+        cancel_url : `${req.get('origin')}/tour/${tour.slug}`,
         customer_email : req.user.email,
         client_reference_id : req.params.tourId,
         line_items : [
